cccb209fa11fb25675325584c1d8be2fcd3d5ca4
Anand32877 <2k22.csai.32877@gmail.com>
2025-10-02 22:35:31 +0530

    Removed hardcoded Hugging Face token, moved to .env

---
+++ src/huggingFace.js
@@ -0,0 +1,19 @@
+
+import { prevUser } from "./context/UserContext";
+
+export async function query() {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
+		{
+			headers: {
+				Authorization: `Bearer ${import.meta.env.VITE_HF_API_KEY}`,
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify({ inputs: prevUser.prompt }),
+		}
+	);
+
+	const result = await response.blob();
+	return result;
+}
